0a7f04e5ae1154680cf8e16e91089a7ca0efc16c
Fabiano V. Santos <santos.fabianov@gmail.com>
2015-08-13 22:56:56 -0300

    Added ZNode tests and name resolver.

--- src/main/java/com/github/gmcoringa/coordinator/core/zookeeper/node/ZNode.java
+++ src/main/java/com/github/gmcoringa/coordinator/core/zookeeper/node/ZNode.java
@@ -4,20 +4,29 @@ import java.util.List;
 
 public class ZNode {
 
-	private final String path;
-	private final List<String> children;
+    private final String path;
+    private final List<String> children;
 
-	public ZNode(String path, List<String> children) {
-		this.path = path;
-		this.children = children;
-	}
+    public ZNode(String path, List<String> children) {
+        this.path = path;
+        this.children = children;
+    }
 
-	public String getPath() {
-		return path;
-	}
+    public String getPath() {
+        return path;
+    }
 
-	public List<String> getChildren() {
-		return children;
-	}
+    public List<String> getChildren() {
+        return children;
+    }
 
+    public String getName() {
+        int index = path.lastIndexOf("/");
+
+        if (index >= 0 && path.length() > 1) {
+            return path.substring(index + 1);
+        }
+
+        return "/";
+    }
 }
