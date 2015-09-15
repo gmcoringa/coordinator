90f5e5187547259e157099c14c0215dcbd248d47
Fabiano V. Santos <santos.fabianov@gmail.com>
2015-09-14 21:48:17 -0300

    Disables suffix-based content negotiation.

---
+++ src/main/java/com/github/gmcoringa/coordinator/core/rest/ContentNegotiationConfiguration.java
@@ -0,0 +1,15 @@
+package com.github.gmcoringa.coordinator.core.rest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+class ContentNegotiationConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
+        // Turn off suffix-based content negotiation
+        configurer.favorPathExtension(false);
+    }
+}
