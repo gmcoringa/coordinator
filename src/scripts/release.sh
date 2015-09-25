af350cdcabe1d2966b0cdea0c1f4718fb8c9d677
Fabiano V. Santos <santos.fabianov@gmail.com>
2015-09-24 23:23:58 -0300

    Setup permission for release.

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -5,6 +5,14 @@ function info {
   printf '\e[1;32m%-6s\e[m\n' " - $1"
 }
 
+info "Setup credentials"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com/gmcoringa/coordinator" > .git/credentials
+
+info "Checkout master"
+git checkout master
+git pull
+
 info "Starting Coodinator Release"
 gradle release \
     -Prelease.useAutomaticVersion=true \
@@ -15,5 +23,8 @@ gradle release \
     -PossrhUsername=$OSSRH_USER \
     -PossrhPassword=$OSSRH_PASS \
     -x test
+
+info "Remove credentials"
+rm -fv .git/credentials
 
 info "Done!"
